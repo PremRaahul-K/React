a5757ef85cfcf0016abf59e54e3400186464fca4
Prem <thisisraahulreddy@gmail.com>
2025-09-13 18:56:54 +0530

    rendering list dynamically

--- Code/react-app/src/components/ListGroup.tsx
+++ Code/react-app/src/components/ListGroup.tsx
@@ -1,13 +1,14 @@
 function ListGroup() {
+  const items = ["Paris", "London", "Canada", "Vizag", "Tirupathi"];
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item, index) => (
+          <li key={index} className="list-group-item">
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
